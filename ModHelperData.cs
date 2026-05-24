8c63df682a43afbb7094bdf8b3670d3657d5d99b
FrostHero <emberherofangames@gmail.com>
2026-05-24 14:35:02 -0500

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+namespace LightMonkey;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "54.0";
+    public const string Version = "1.1.0";
+    public const string Name = "LightMonkey";
+
+    public const string Description = "The Light Monkey mod! Allows you to put down the Light Monkey to eliminate the bloons!";
+
+    public const string RepoOwner = "FrostHero"; 
+    public const string RepoName = "LightMonkey";
+}
